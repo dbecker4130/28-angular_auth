978bd664066a6569a3b7c6dcb5fd83a09110e67e
Daniel Becker <dbecker4130@gmail.com>
2017-01-30 12:07:49 -0800

    gallery display styling complete

--- app/view/home/home-controller.js
+++ app/view/home/home-controller.js
@@ -1,9 +1,22 @@
 'use strict';
 
-// require('./_home.scss');
+module.exports = ['$log', '$rootScope', 'galleryService', HomeController];
 
-module.exports = ['$log', HomeController];
-
-function HomeController($log) {
+function HomeController($log, $rootScope, galleryService) {
   $log.debug('HomeController');
+
+  this.galleries = [];
+
+  this.fetchGalleries = function() {
+    galleryService.fetchGalleries()
+    .then( galleries => {
+      this.galleries = galleries;
+    });
+  };
+
+  this.fetchGalleries();
+
+  $rootScope.$on('locationChangeSuccess', () => {
+    this.fetchGalleries();
+  });
 }
