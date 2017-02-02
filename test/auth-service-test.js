0a537f31dc4cd871f552d968b3e88be51709b7d0
Daniel Becker <dbecker4130@gmail.com>
2017-02-02 11:03:47 -0800

    3 tests from class pass

---
+++ test/auth-service-test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('Auth Service', function() {
+  beforeEach(() => {
+    angular.mock.module('cfgram');
+    angular.mock.inject(( $rootScope, authService, $window, $httpBackend) => {
+      this.$window = $window;
+      this.$rootScope = $rootScope;
+      this.authService = authService;
+      this.$httpBackend = $httpBackend;
+    });
+  });
+
+  describe('authService.getToken()', () => {
+    it('should return a token', () => {
+      this.authService.token = null;
+      this.$window.localStorage.setItem('token', 'test token');
+
+      this.authService.getToken()
+      .then( token => {
+        expect(token).toEqual('test token');
+      })
+      .catch( err => {
+        expect(err).toEqual(null);
+      });
+
+      this.$rootScope.$apply();
+    });
+  });
+
+  // describe('authService.logout()', () => {
+  //   it('should remove a token', () => {
+  //
+  //     this.authService.getToken();
+  //     this.$window.localStorage.removeItem('token', 'test token');
+  //
+  //     this.authService.logout()
+  //     .then( token => {
+  //       expect(token).toEqual(null);
+  //     });
+  //   });
+  // });
+});
