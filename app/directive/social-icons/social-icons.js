33dba9f8ca0606388ee087bdd3f99fc2ff20f226
Daniel Becker <dbecker4130@gmail.com>
2017-02-08 11:00:50 -0800

    sprite added to footer

--- app/directive/social-icons/social-icons.js
+++ app/directive/social-icons/social-icons.js
@@ -1,12 +1,14 @@
 'use strict';
 
+require('./_social-icons.scss');
+
 module.exports = function() {
   return {
     restrict: 'EAC',
     template: require('./social-icons.html'),
     controller: ['$log', SocialIconsController],
-    controllerAs: 'socialIconsCtrl',
     bindToController: true,
+    controllerAs: 'socialIconsCtrl',
     scope: {
       title: '@'
     }
